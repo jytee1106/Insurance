1292ac950ee1ef31f5cc470951abaf6c7acf7ad8
anreh <anurehka@gmail.com>
2019-11-11 14:03:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/insurance/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.insurance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(),
+    AdapterView.OnItemSelectedListener {
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Toast.makeText(this,"Position :"+ position,Toast.LENGTH_LONG).show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        spinnerAge.onItemSelectedListener = this
+        buttonCalculate.setOnClickListener {
+            calculatePremium()
+        }
+    }
+
+    private fun calculatePremium() {
+        val position = spinnerAge.selectedItemPosition
+        val gender = radioGroupGender.checkedRadioButtonId
+        if(gender == R.id.radioButtonMale){
+            //TODO calculate premium for male
+        }
+
+        if(checkBoxSmoker.isChecked){
+            //TODO calculate premium for smoke
+        }
+    }
+}
